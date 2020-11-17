c9b5dc72893e77af1036d39a2011c0e2b3208eac
brunobernal <67240110+brunobernal@users.noreply.github.com>
2020-11-17 15:57:53 -0300

    data structure

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
 const mixedMessagesJavaScript = () => {
+
+    const randomMessage = () => {
+        
+        const data = {
+            object: ["Array","String","Math"],
+            difficulty: ["easy","medium","hard"]
+            arrayMethods: {
+                easy: [],
+                medium: [],
+                hard: []},
+            stringMethods: {
+                easy: [],
+                medium: [],
+                hard: []},
+            mathMethods: {
+                easy: [],
+                medium: [],
+                hard: []},
+        }
+
+
+    }
+
+
     console.log('test')
 };
 mixedMessagesJavaScript();
